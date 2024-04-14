fdf05f32752433105151cd30adb63e8ee76f603a
Eric Salavat <eric.salavat@gmail.com>
2024-04-13 21:42:39 -0700

    Added 2nd paddle and ball.

--- game.py
+++ game.py
@@ -9,30 +9,61 @@ window = pygame.display.set_mode((800, 600))
 
 
 class Paddle(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y, up, down):
         super(Paddle, self).__init__()
         self.height = 75
         self.width = 20
         self.surf = pygame.Surface((self.width, self.height))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
-        self.x = 10
-        self.y = SCREEN_HEIGHT / 2 - (self.height/2)
+        self.x = x
+        self.y = y - (self.height / 2)
+        self.up = up
+        self.down = down
 
 
     def update(self, keys_pressed):
-        if(keys_pressed[pygame.K_UP]):
+        if(keys_pressed[self.up]):
             self.y -= 1
-        if(keys_pressed[pygame.K_DOWN]):
+        if(keys_pressed[self.down]):
             self.y += 1
         if(self.y <= 0):
             self.y = 0
         if(self.y + self.height >= SCREEN_HEIGHT):
             self.y = SCREEN_HEIGHT - self.height
 
-gameObjects = {}
-gameObjects["player"] = Paddle()
 
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Ball, self).__init__()
+        self.height = 20
+        self.width = 20
+        self.surf = pygame.Surface((self.width, self.height))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        self.x = SCREEN_WIDTH / 2 - (self.width / 2)
+        self.y = SCREEN_HEIGHT / 2 - (self.height / 2)
+        self.velX = 1
+        self.velY = 1
+
+    def update(self, keys_pressed):
+        if(self.x <= 0):
+            self.velX = 1
+        if(self.x + self.width >= SCREEN_WIDTH):
+            self.velX = -1
+        if(self.y <= 0):
+            self.velY = 1
+        if(self.y + self.height >= SCREEN_HEIGHT):
+            self.velY = -1
+
+        self.x += self.velX
+        self.y += self.velY
+        
+
+gameObjects = {}
+gameObjects["player1"] = Paddle(10, SCREEN_HEIGHT/2, pygame.K_q, pygame.K_a)
+gameObjects["player2"] = Paddle(770, SCREEN_HEIGHT/2, pygame.K_UP, pygame.K_DOWN)
+gameObjects["ball"] = Ball()
 
 # Wait for the user to quit
 while True:
@@ -43,7 +74,6 @@ while True:
 
     pressed_keys = pygame.key.get_pressed()
     
-        
 
     window.fill((0, 0, 0))
 
